8869577697d2714e7c51b0c2bc2cb26bd3f61288
Vidito <56190096+Vidito@users.noreply.github.com>
2021-05-26 19:42:48 +0200

    third

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -1,7 +1,7 @@
 """personal_portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,14 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
-from django.views.static import serve
-from django.conf.urls import url 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
+    path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
